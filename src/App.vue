8451ecd0dad0a520ffce3b878c086d5d606eebc0
miki oyanagi <mikioyanagi.0806@icloud.com>
2021-09-05 21:14:56 +0900

    done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
+    <Login></Login>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
-    <router-view/>
+    <!-- <router-view/> -->
   </div>
 </template>
+
+<script>
+import Login from "./components/Login.vue";
+export default {
+  components:{
+    Login
+}
+}
+</script>
 
 <style>
 #app {
